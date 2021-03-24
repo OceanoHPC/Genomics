a9377e2c86d98c993eb01ee851a88311db734816
Margarita3048 <80792310+Margarita3048@users.noreply.github.com>
2021-03-24 15:04:00 -0500

    Delete copy.sh

--- copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -J Transfer_4_work
-#SBATCH -p CPU               # Partición o cola
-#SBATCH --nodes=1             # Numero de nodos
-#SBATCH --ntasks-per-node=1   # Numero de cores
-#SBATCH --mem=2000            # Bloque de memoria para todos los nodos
-#SBATCH --time=96:00          # Duración (D-HH:MM)
-cp -r /home/jose.gallardo/NGS_dat/4_Wild_Norway_12/ /work/jose.gallardo/
